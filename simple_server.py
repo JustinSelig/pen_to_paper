75f0a85a959145d63850d4c1948d7e24159c4b81
Justin Selig <jss459@cornell.edu>
2017-05-06 20:15:54 -0400

    working on motor control

---
+++ simple_server.py
@@ -0,0 +1,91 @@
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+import time
+import atexit
+import threading
+import random
+import argparse
+import math
+import msgpackrpc
+import socket
+import RPi.GPIO as GPIO
+
+PWM_PIN = 22
+INCREASE = 1
+DECREASE = -1
+
+
+def stepper_worker(stepper, numsteps, direction, style):
+	stepper.step(numsteps, direction, style)
+
+def tick(stepper, direction, steps=1):
+	worker = threading.Thread(target=stepper_worker, args=(stepper, steps, direction, Adafruit_MotorHAT.DOUBLE))
+	worker.start()
+	while worker.isAlive():
+		pass
+
+def tick_async(stepper, direction, steps=1):
+	worker = threading.Thread(target=stepper_worker, args=(stepper, steps, direction, Adafruit_MotorHAT.DOUBLE))
+	worker.start()
+	return worker
+
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+
+class SimpleServer(object):
+	def __init__(self, left, right, servo):
+		self.servo = servo
+		self.left = left
+		self.right = right
+
+	def pen_down(self):
+		self.servo.start(20/10)
+		time.sleep(.25)
+		self.servo.ChangeDutyCycle(0)
+		print "pen down"
+
+	def tick_right(self, direction):
+		print "tick right"
+		if direction == INCREASE:
+			tick(self.right, Adafruit_MotorHAT.FORWARD)
+			self.total_right += 1
+		elif direction == DECREASE:
+			tick(self.right, Adafruit_MotorHAT.BACKWARD)
+			self.total_right -= 1
+
+if __name__ == '__main__':
+	# create a default object, no changes to I2C address or frequency
+	mh = Adafruit_MotorHAT()
+
+	atexit.register(turnOffMotors)
+
+	stepper_left = mh.getStepper(200, 1)          # 200 steps/rev, motor port #1
+	stepper_right = mh.getStepper(200, 2)         # 200 steps/rev, motor port #1
+	stepper_left.setSpeed(200)            # 30 RPM
+	stepper_right.setSpeed(200)           # 30 RPM
+
+	st1 = threading.Thread(target=stepper_worker, args=(stepper_left, 400, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE))
+	st1.start()
+
+	st2 = threading.Thread(target=stepper_worker, args=(stepper_right, 400, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE))
+	st2.start()
+
+	# Wait for it to center
+	while st1.isAlive() or st2.isAlive():
+		time.sleep(1)
+
+	# Configure input pins with internal pull up resistors
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(PWM_PIN, GPIO.OUT)
+
+	# Set to 50Hz
+	p = GPIO.PWM(PWM_PIN, 10)
+
+	server = msgpackrpc.Server(SimpleServer(stepper_left, stepper_right, p))
+	server.listen(msgpackrpc.Address("localhost", 18800))
+	server.start()
+
+
--- test_draw.py
+++ test_draw.py
@@ -4,92 +4,77 @@ import signal
 import RPi.GPIO as GPIO
 import time
 import os
-
-#os.putenv('SDL_VIDEODRIVER', 'fbcon') # Display on piTFT
-os.putenv('SDL_FBDEV', '/dev/fb1') #
-os.putenv('SDL_MOUSEDRV', 'TSLIB') # Track mouse clicks on piTFT
-os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
-
-pygame.init()
-
-size = width, height = 320, 240
-WHITE = (255,255,255)
-#pigame stuff
-pygame.mouse.set_visible(False)
-screen = pygame.display.set_mode(size)
-
-
+import msgpackrpc
 import random
 
 #screen = pygame.display.set_mode((800,600))
+global client
 
-draw_on = False
-last_pos = (0, 0)
-color = (255, 128, 0)
-radius = 10
+def draw_to_paper(dx, dy):
+    global client
+    client.call('pen_down')
 
+"""
+Below code adapted from Stack Overflow post:
+http://stackoverflow.com/questions/597369/how-to-create-ms-paint-clone-with-python-and-pygame/5900836
+"""
 def roundline(srf, color, start, end, radius=1):
     dx = end[0]-start[0]
     dy = end[1]-start[1]
+    print dx, dy
     distance = max(abs(dx), abs(dy))
     for i in range(distance):
         x = int( start[0]+float(i)/distance*dx)
         y = int( start[1]+float(i)/distance*dy)
         pygame.draw.circle(srf, color, (x, y), radius)
+    return (dx, dy)
 
-try:
-    while True:
-        e = pygame.event.wait()
-        if e.type == pygame.QUIT:
-            raise StopIteration
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            color = (random.randrange(256), random.randrange(256), random.randrange(256))
-            pygame.draw.circle(screen, color, e.pos, radius)
-            draw_on = True
-        if e.type == pygame.MOUSEBUTTONUP:
-            draw_on = False
-        if e.type == pygame.MOUSEMOTION:
-            if draw_on:
-                pygame.draw.circle(screen, color, e.pos, radius)
-                roundline(screen, color, e.pos, last_pos,  radius)
-            last_pos = e.pos
-        pygame.display.flip()
+def main():
+	global client
+	client = msgpackrpc.Client(msgpackrpc.Address("localhost", 18801)) #next, accept host as argument and port
 
-except StopIteration:
-    pass
+	#uncomment below line to run on tft screen
+	#os.putenv('SDL_VIDEODRIVER', 'fbcon') # Display on piTFT
+	os.putenv('SDL_FBDEV', '/dev/fb1') #
+	os.putenv('SDL_MOUSEDRV', 'TSLIB') # Track mouse clicks on piTFT
+	os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
-pygame.quit()
+	pygame.init()
 
-"""
-my_font = pygame.font.Font(None, 20)
-x,y=None,None
-my_buttons = {'QUIT':(80, 180), 'Touch at: '+str(x)+','+str(y):(160,120)}
+	size = width, height = 320, 240
+	WHITE = (255,255,255)
+	#pigame stuff
+	pygame.mouse.set_visible(False)
+	screen = pygame.display.set_mode(size)
 
- 
-while True:
-	time.sleep(0.1)
-	prev_x, prev_y = x,y
-	for event in pygame.event.get():
-		if (event.type is MOUSEBUTTONDOWN):
-			print True
-			pos = pygame.mouse.get_pos()
-			x,y = pos
-			print "Touch at: " + str(x)+','+str(y)	
-			my_buttons['Touch at: '+str(x)+','+str(y)] = my_buttons.pop('Touch at: '+str(prev_x)+','+str(prev_y))
-			screen.fill((0,0,0))
-			if y>140:
-				if x<160:
-					print "quit button pressed"
-					pygame.quit()
-					quit()
-				else:
-					print "outside of button pressed"
-			prev_x, prev_y = x,y
-	
-	
-	for my_text,  text_pos in my_buttons.items():
-                text_surface = my_font.render(my_text, True, WHITE)
-                rect = text_surface.get_rect(center=text_pos)
-                screen.blit(text_surface, rect)
-        pygame.display.flip()
-"""
+	draw_on = False
+	last_pos = (0, 0)
+	color = (255, 128, 0)
+	radius = 1
+
+	try:
+	    while True:
+	        e = pygame.event.wait()
+	        if e.type == pygame.QUIT:
+	            raise StopIteration
+	        if e.type == pygame.MOUSEBUTTONDOWN:
+	            color = (random.randrange(256), random.randrange(256), random.randrange(256))
+	            pygame.draw.circle(screen, color, e.pos, radius)
+	            draw_on = True
+	        if e.type == pygame.MOUSEBUTTONUP:
+	            draw_on = False
+	        if e.type == pygame.MOUSEMOTION:
+	            if draw_on:
+	                pygame.draw.circle(screen, color, e.pos, radius)
+	                (motor_dx, motor_dy) = roundline(screen, color, e.pos, last_pos, radius)
+			draw_to_paper(motor_dx, motor_dy)
+	            last_pos = e.pos
+	        pygame.display.flip()
+
+	except StopIteration:
+	    pass
+
+	pygame.quit()
+
+if __name__ == "__main__":
+	main()
